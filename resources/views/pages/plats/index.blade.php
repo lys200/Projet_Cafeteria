4d5be7d3d5097b35d3fdaeeb7ef12beee6886217
mienove <samienovebelceus07@gmail.com>
2025-08-25 19:42:27 -0500

    test et affichage

---
+++ resources/views/pages/plats/index.blade.php
@@ -0,0 +1,50 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Plat</title>
+</head>
+<body>
+    <h1>Liste des plats</h1>
+    <a href="{{route('plat.create')}}">ajouter un plat</a>
+
+    <table>
+        <thead>
+
+            <tr>
+            <th>Code_plat</th>
+            <th>Nom_Plat</th>
+            <th>Cuisson</th>
+            <th>Prix</th>
+            <th>Quantité </th>
+            <th>Disponible_jour</th>
+            <th>Description</th>
+            <th>Action</th>
+            </tr>
+        </thead>
+        <tbody>
+            @foreach ($plats as $plat)
+            <tr>
+                <td>{{$plat->code_plat}}</td>
+                <td>{{$plat->nom_plat}}</td>
+                <td>{{$plat->cuisson}}</td>
+                <td>{{$plat->prix}}</td>
+                <td>{{$plat->quantite}}</td>
+                <td>{{$plat->disponible_jour}}</td>
+                <td>{{$plat->description}}</td>
+                <td>
+                    <a href="#">Modifier</a>|
+                    <a href="#">Supprimer</a>
+
+                </td>
+
+            </tr>
+            @endforeach
+        </tbody>
+    </table>
+
+</body>
+</html>
+
--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\PlatController;
 
 Route::get('/', function () {
     return view('welcome');
@@ -11,10 +12,29 @@ Route::get('/dashboard', function () {
     return view('dashboard');
 })->middleware(['auth', 'verified'])->name('dashboard');
 
+Route::name('plat.')->controller(PlatController::class)
+    ->group(function(){
+
+        Route::get('/', 'index')->name('index.plat');
+        Route::get('/edit/{$id}', ['edit'])->name('index.plat');
+        Route::get('/', 'index')->name('index.plat');
+        Route::get('/', 'index')->name('index.plat');
+
+    });
+// route pour les ventes
+Route::resource('vente', VenteController::class);
+
+// route pour les plats
+// Route::resource('plat', PlatController::class);
+
+// route pour les clients
+Route::resource('client', ClientController::class);
+
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
+
 
 require __DIR__.'/auth.php';
